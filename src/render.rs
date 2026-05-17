f958aae5fc004b9c956d4b40b44d12995400ecd7
PabloCorzo <pablo.corzocorella@usp.ceu.es>
2026-05-17 10:52:30 +0200

    render improvement

--- src/render.rs
+++ src/render.rs
@@ -2,18 +2,17 @@ use crate::board::{BoardState, get_icon};
 
 pub fn render_board_cli(board: &BoardState){
 
+    println!("-----------------------------------------");
     for i in 0..8{
         //16 long
         // println!("----------------");
-        //40
-        println!("----------------------------------------");
+        //41
         let mut buffer = String::new();
         buffer.push_str("| ");
         for j in 0..8{
             buffer.push_str(&format!("{}  | ", get_icon(&board.piece_at((7 - i,j)))));
         }
         println!("{}",buffer);
-        println!("----------------------------------------");
-
+        println!("-----------------------------------------");
     }
 }
